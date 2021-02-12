8e8eb4ad0d80838b80f1aa41b63aa68ddf736bcf
Raghav Parashar <72915932+RaghavParashar1@users.noreply.github.com>
2021-02-13 00:03:58 +0530

    Add files via upload

---
+++ Day_1_3.js
@@ -0,0 +1,8 @@
+function main() {
+    const PI = Math.PI;
+    var r = parseFloat(readLine());
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    console.log(PI*r*r);
+    // Print the area of the circle:
+    console.log(2*PI*r);
+    // Print the perimeter of the circle:
