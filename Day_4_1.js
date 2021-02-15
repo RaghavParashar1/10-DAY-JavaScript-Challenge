0b505f91054affa1b6275e0f2e9b1db08a6260d3
Raghav Parashar <72915932+RaghavParashar1@users.noreply.github.com>
2021-02-16 00:19:01 +0530

    Add files via upload

---
+++ Day_4_1.js
@@ -0,0 +1,9 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    var c=2*(a+b)
+    var ar=a*b
+    var ob={"length":a,"width":b,"perimeter":c,"area":ar}
+    return ob
+}
